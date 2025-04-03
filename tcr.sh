dc3ee9bf4127a83d0a00f923afa621d95ae0b153
Alex Szabo <kreatemore@gmail.com>
2025-04-03 22:33:26 +0200

    more idiomatic TCR script

--- tcr.sh
+++ tcr.sh
@@ -1,6 +1,3 @@
 cargo clippy || exit 1
 
-git add -A
-git commit -m "$1"
-
-cargo test && git push || git reset --hard
+cargo test && git add -A && git commit -m "$1" && git push || git reset --hard
